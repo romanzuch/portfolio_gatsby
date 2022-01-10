d6f507fae7b6b2f1f4e7e4074e4fd6c5de5348e5
Roman <roman.zuchowski@gmail.com>
2022-01-10 18:18:17 +0100

    💡 Added some basic styles for the resume page

---
+++ src/styles/resume.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Station = styled.div`
+    display: flex;
+    align-items: left;
+    padding: 0;
+    margin: 0;
+`
+
+export const Info = styled.div`
+    padding: 2rem;
+`
+
+export const Employer = styled.h1`
+
+`
+
+export const Position = styled.div`
+    padding: 2rem;
+`
+
+export const Role = styled.h2`
+
+`
