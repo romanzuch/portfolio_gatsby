2df19f1e4567ecca0f356c73847b4d7d04104ae9
Roman <roman.zuchowski@gmail.com>
2022-01-10 18:18:37 +0100

    💡 Defined structure for the resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { Station, Info, Employer, Role, Position } from '../styles/resume';
 
 export default function Resume() {
     return (
         <div>
             <Layout>
                 <h1>resume</h1>
+                <Station>
+                    <Info>
+                        <Employer>medloop</Employer>
+                        {/* here comes some info similar to LinkedIn */}
+                    </Info>
+                    <Position>
+                        <Role>product manager</Role>
+                    </Position>
+                </Station>
             </Layout>
         </div>
     )
