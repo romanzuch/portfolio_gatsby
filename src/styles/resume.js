cd82f5654052569ef9fc0f8cda3b02298fdc08b1
Roman <roman.zuchowski@gmail.com>
2022-01-13 20:25:36 +0100

    💡Added styled components for resume

--- src/styles/resume.js
+++ src/styles/resume.js
@@ -1,24 +1,52 @@
 import styled from 'styled-components';
 
-export const Station = styled.div`
-    display: flex;
-    align-items: left;
-    padding: 0;
-    margin: 0;
+export const ResumeHeader = styled.h1`
+    margin: 0 0 1rem 0;
 `
 
 export const Info = styled.div`
-    padding: 2rem;
+    padding: 1rem 3rem 0 0;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
 `
 
-export const Employer = styled.h1`
+export const InfoElement = styled.p`
+    color: grey;
+    margin: 0;
+    padding: 0.2rem 0 0 0;
+`
 
+export const Employer = styled.h2`
+    font-size: 1.4rem;
+    margin: 0;
+`
+
+export const Positions = styled.div`
+    display: flex;
+    flex-direction: column;
 `
 
 export const Position = styled.div`
-    padding: 2rem;
+    padding: 1rem 0 1rem 0;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
 `
 
 export const Role = styled.h2`
+    font-size: 1.4rem;
+`
 
+export const RoleDescription = styled.ul`
+    list-style-type: circle; 
+    margin: 0.5rem 0 0 0;
+    padding: 0;
+    text-align: start;
+`
+
+export const RoleDescriptionElement = styled.li`
+    margin: 0;
+    padding: 0.5rem;
+    
 `
