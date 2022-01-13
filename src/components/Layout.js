31b40834375fea761727a1ebb4d390b489bfa337
Roman <roman.zuchowski@gmail.com>
2022-01-13 20:51:19 +0100

    💡 Added a button that indicates scrollable content

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import NavigationBar from './Navbar';
 
 import Socials from "./socials";
+import ScrollDownButton from './scrollDownButton'
 
 const LayoutMain = styled.main `
     margin: 5rem 5rem 0 5rem;
@@ -18,6 +19,7 @@ const Layout = ({children}) => {
             <NavigationBar />
             <LayoutMain>
                 {children}
+                <ScrollDownButton />
                 <Socials />
             </LayoutMain>
         </>
