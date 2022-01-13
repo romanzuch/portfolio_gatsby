ae4518468bf0989b18add43761ab2c78878588bc
Roman <roman.zuchowski@gmail.com>
2022-01-13 20:24:41 +0100

    💡Embedded resume into mui grid to make it responsive

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,21 +1,144 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import { Station, Info, Employer, Role, Position } from '../styles/resume';
+import { ResumeHeader } from '../styles/resume';
+import { Info, InfoElement } from '../styles/resume';
+import { Employer, Role, Positions, Position } from '../styles/resume';
+import { RoleDescription, RoleDescriptionElement } from '../styles/resume';
+
+import Grid from '@mui/material/Grid';
 
 export default function Resume() {
     return (
         <div>
             <Layout>
-                <h1>resume</h1>
-                <Station>
-                    <Info>
-                        <Employer>medloop</Employer>
-                        {/* here comes some info similar to LinkedIn */}
-                    </Info>
-                    <Position>
-                        <Role>product manager</Role>
-                    </Position>
-                </Station>
+                <ResumeHeader>resume</ResumeHeader>
+
+                <Grid container spacing={3}>
+
+                    { /* station 1 */}
+                    <Grid item xs={3}>
+                        <Info>
+                            <Employer>medloop</Employer>
+                            {/* here comes some info similar to LinkedIn */}
+                            <InfoElement>2020/09 - today</InfoElement>
+                            <InfoElement>berlin, germany</InfoElement>
+                        </Info>
+                    </Grid>
+
+                    <Grid item xs={9}>
+                        <Positions>
+                            <Position>
+                                {/* position 1 */}
+                                <Role>product manager</Role>
+                                <InfoElement>2021/09 - today</InfoElement>
+                                <RoleDescription>
+                                    <RoleDescriptionElement>development of a seamless patient experience to ensure easier access to health care</RoleDescriptionElement>
+                                    <RoleDescriptionElement>advancements to our services to digitalize and improve the patient journey</RoleDescriptionElement>
+                                    <RoleDescriptionElement>carrying out client/user research</RoleDescriptionElement>
+                                    <RoleDescriptionElement>creating workflows, wireframes, mockups, and user stories</RoleDescriptionElement>
+                                    <RoleDescriptionElement>technical onboarding of new colleagues, including setting up keys and vpn connections</RoleDescriptionElement>
+                                    <RoleDescriptionElement>basically the go-to IT admin + support for the whole company</RoleDescriptionElement>
+                                    <RoleDescriptionElement>used technology: whimsical, figma, jira, confluence</RoleDescriptionElement>
+                                </RoleDescription>
+                            </Position>
+                            <Position>
+                                {/* position 2 */}
+                                <Role>technical account manager</Role>
+                                <InfoElement>2020/09 - 2021/09</InfoElement>
+                                <RoleDescription>
+                                    <RoleDescriptionElement>responsible for the implementation and the operation of PMS software</RoleDescriptionElement>
+                                    <RoleDescriptionElement>responsible for the technical realization on-site, training of the practice staff, and for technical support</RoleDescriptionElement>
+                                    <RoleDescriptionElement>involved in the development of the PMS software as well as our patient app</RoleDescriptionElement>
+                                    <RoleDescriptionElement>used technology: swift, uikit, reactjs, linux</RoleDescriptionElement>
+                                </RoleDescription>
+                            </Position>
+                        </Positions>
+                    </Grid>
+
+                    { /* station 2 */}
+                    <Grid item xs={3}>
+                        <Info>
+                            <Employer>edag group</Employer>
+                            {/* here comes some info similar to LinkedIn */}
+                            <InfoElement>2018/05 - 2020/09</InfoElement>
+                            <InfoElement>munich, germany</InfoElement>
+                        </Info>
+                    </Grid>
+
+                    <Grid item xs={9}>
+                        <Positions>
+                            <Position>
+                                {/* position 3 */}
+                                <Role>systems engineer</Role>
+                                <InfoElement>2020/04 - 2020/09</InfoElement>
+                                <RoleDescription>
+                                    <RoleDescriptionElement>ecu software testing for large MAN trucks</RoleDescriptionElement>
+                                    <RoleDescriptionElement>software test automation</RoleDescriptionElement>
+                                </RoleDescription>
+                            </Position>
+                            <Position>
+                                {/* position 4 */}
+                                <Role>development engineer</Role>
+                                <InfoElement>2018/05 - 2020/04</InfoElement>
+                                <RoleDescription>
+                                    <RoleDescriptionElement>responsible engineering lead of a inter-disciplinary team for the BMW ag</RoleDescriptionElement>
+                                    <RoleDescriptionElement>continuous engineering of a occupant detection system</RoleDescriptionElement>
+                                    <RoleDescriptionElement>coordination between various internal and external partners, as well as suppliers</RoleDescriptionElement>
+                                </RoleDescription>
+                            </Position>
+                        </Positions>
+                    </Grid>
+
+                    { /* station 3 */}
+                    <Grid item xs={3}>
+                        <Info>
+                            <Employer>siemens</Employer>
+                            {/* here comes some info similar to LinkedIn */}
+                            <InfoElement>2017/05 - 2017/08</InfoElement>
+                            <InfoElement>mülheim, germany</InfoElement>
+                        </Info>
+                    </Grid>
+
+                    <Grid item xs={9}>
+                        <Positions>
+                            <Position>
+                                {/* position 5 */}
+                                <Role>graduate</Role>
+                                <InfoElement>2017/05 - 2017/08</InfoElement>
+                                <RoleDescription>
+                                    <RoleDescriptionElement>bachelor thesis: "phase change of partially stabilized zirconia under variable temperature load conditions"</RoleDescriptionElement>
+                                </RoleDescription>
+                            </Position>
+                        </Positions>
+                    </Grid>
+
+                    { /* station 4 */}
+                    <Grid item xs={3}>
+                        <Info>
+                            <Employer>siemens</Employer>
+                            {/* here comes some info similar to LinkedIn */}
+                            <InfoElement>2016/08 - 2016/12</InfoElement>
+                            <InfoElement>mülheim, germany</InfoElement>
+                        </Info>
+                    </Grid>
+
+                    <Grid item xs={9}>
+                        <Positions>
+                            <Position>
+                                {/* position 6 */}
+                                <Role>intern</Role>
+                                <InfoElement>2016/08 - 2016/12</InfoElement>
+                                <RoleDescription>
+                                    <RoleDescriptionElement>collaboration in inter-disciplinary team to improve ceramic-based turbine coatings</RoleDescriptionElement>
+                                    <RoleDescriptionElement>preparation and presentation of lectures</RoleDescriptionElement>
+                                    <RoleDescriptionElement>creatioin of literature organisation tool using excel vba</RoleDescriptionElement>
+                                </RoleDescription>
+                            </Position>
+                        </Positions>
+                    </Grid>
+
+                </Grid>
+
             </Layout>
         </div>
     )
