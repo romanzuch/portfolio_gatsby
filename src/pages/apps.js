956acbbfa6048c346f30f6247b99ee8335f94dad
Roman <roman.zuchowski@gmail.com>
2022-01-15 18:33:54 +0100

    💡Implemented basic structure for page

--- src/pages/apps.js
+++ src/pages/apps.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { useEffect, useState } from "react";
+import Grid from '@mui/material/Grid';
+import { Constants } from '../components/Constants';
+import { AppsHeader } from '../styles/apps';
 
 export default function Apps() {
+
+    useEffect(() => {
+        document.title = 'romanzuch · apps'
+    }, []);
+
     return (
         <div>
             <Layout>
-                <h1>apps</h1>
+                <AppsHeader>apps</AppsHeader>
+
+                <Grid container spacing={0}>
+
+                    { /* app 1 */}
+                    <Grid item 
+                        xs={Constants.gridSizing.small.left} 
+                        md={Constants.gridSizing.medium.left} 
+                        lg={Constants.gridSizing.large.left}
+                    >
+
+                        <h2>App Image/s</h2>
+
+                    </Grid>
+
+                    <Grid item 
+                        xs={Constants.gridSizing.small.right} 
+                        md={Constants.gridSizing.medium.right} 
+                        lg={Constants.gridSizing.large.right}
+                    >
+
+                        <h2>App Info Card</h2>
+
+                    </Grid>
+
+                </Grid>
+
             </Layout>
         </div>
     )
