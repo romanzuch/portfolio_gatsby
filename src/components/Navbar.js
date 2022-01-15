5764756d8b10204f7457314d4e038efbb168849d
Roman <roman.zuchowski@gmail.com>
2022-01-15 18:33:08 +0100

    💡Rearranged the code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 
+const Navbar = styled.nav `
+    margin: 0;
+    padding-top: 2rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+`
+
 const NavbarLink = styled(Link) `
     padding: 1;
     margin: 10px; 
     color: grey;
     text-decoration: none;
-`
-
-const Navbar = styled.nav `
-    margin-top: 2rem;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
 `
 
 const NavigationBar = () => {
