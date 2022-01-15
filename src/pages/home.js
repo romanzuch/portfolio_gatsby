6e2f16cb273dbb92a62cf0ef84f822e693c5ae1e
Roman <roman.zuchowski@gmail.com>
2022-01-15 18:34:31 +0100

    💡Changing window title on render

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useEffect } from "react";
 
 export default function Home() {
+
+    useEffect(() => {
+        document.title = 'romanzuch · home'
+    }, []);
+
     return (
         <div>
 	        <h1>my name is roman.</h1>
