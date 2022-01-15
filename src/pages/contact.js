9de64dc607cbb80bc11611a713017a31c124aa8f
Roman <roman.zuchowski@gmail.com>
2022-01-15 18:34:19 +0100

    💡Changing window title on render

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { useEffect } from "react";
 
 export default function Contact() {
+
+    useEffect(() => {
+        document.title = 'romanzuch · contact'
+    }, []);
+
     return (
         <div>
             <Layout>
