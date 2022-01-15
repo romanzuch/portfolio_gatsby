254134e35cf536f1fc157b8c2f0e3c4aea6e44a0
Roman <roman.zuchowski@gmail.com>
2022-01-15 21:20:15 +0100

    💡 Initial commit home.js styles

---
+++ src/styles/home.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+export const Header = styled.div `
+    margin: 2rem 0 0 0;
+`
+
+export const Description = styled.div `
+    margin: 5rem 0 5rem 0;
+    width: 80vh;
+`
+
+export const DescriptionText = styled.p `
+    
+`
