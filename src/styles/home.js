4e065f4882e5b9f482b6dd4d8788dd02d3ab7d01
Roman <roman.zuchowski@gmail.com>
2022-01-15 21:40:21 +0100

    💡Added styling to home page

--- src/styles/home.js
+++ src/styles/home.js
@@ -1,13 +1,25 @@
 import styled from "styled-components";
 
 
-export const Header = styled.div `
+export const SiteHeader = styled.div `
     margin: 2rem 0 0 0;
+`
+
+export const HeaderLarge = styled.h1 `
+    padding: 0.5rem;
+`
+
+export const HeaderMedium =  styled.h2 `
+    padding: 1rem;
+`
+
+export const HeaderSmall = styled.h3 `
+    padding: 1.5rem;
 `
 
 export const Description = styled.div `
     margin: 5rem 0 5rem 0;
-    width: 80vh;
+    width: 80vw;
 `
 
 export const DescriptionText = styled.p `
