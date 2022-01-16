7bdcd51ce50a5cb48c8faa4f8884c4134e3d88c1
Roman <roman.zuchowski@gmail.com>
2022-01-16 09:02:03 +0100

    ➕Added conditional scroll indicator

--- src/pages/websites.js
+++ src/pages/websites.js
@@ -3,13 +3,55 @@ import Layout from '../components/Layout';
 import { useEffect, useState } from "react";
 import Grid from '@mui/material/Grid';
 import { Constants } from '../components/Constants';
+import ScrollDownButton from '../components/scrollDownButton';
 
 import { WebsitesHeader } from '../styles/websites';
 
 const Websites = () => {
 
+    const isScrollable = function (ele) {
+        // Compare the height to see if the element has scrollable content
+        const hasScrollableContent = ele.scrollHeight > ele.clientHeight;
+    
+        // It's not enough because the element's `overflow-y` style can be set as
+        // * `hidden`
+        // * `hidden !important`
+        // In those cases, the scrollbar isn't shown
+        const overflowYStyle = window.getComputedStyle(ele).overflowY;
+        const isOverflowHidden = overflowYStyle.indexOf('hidden') !== -1;
+    
+        return hasScrollableContent && !isOverflowHidden;
+    };
+
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const [scrollable, setScrollable] = useState();
+
+    const handleScroll = () => {
+        const position = window.pageYOffset;
+        setScrollPosition(position);
+    };
+
+    function ScrollIndicator() {
+        if (scrollable) {
+            if (scrollPosition < 50) {
+                return <ScrollDownButton />
+            } else {
+                return <div />
+            }
+        } else {
+            return <div />
+        }
+    };
+
     useEffect(() => {
         document.title = 'romanzuch · websites'
+
+        setScrollable(isScrollable(document.body));
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
     }, []);
 
     return (
@@ -40,7 +82,7 @@ const Websites = () => {
                 </Grid>
 
             </Grid>
-
+            <ScrollIndicator />
         </Layout>
     );
 };
