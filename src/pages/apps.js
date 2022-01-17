50b922c2cede10584cd14efb8209aaae70a77063
Roman <roman.zuchowski@gmail.com>
2022-01-17 20:06:43 +0100

    💡 Refactored for change from styled-component

--- src/pages/apps.js
+++ src/pages/apps.js
@@ -3,8 +3,9 @@ import Layout from '../components/Layout';
 import { useEffect, useState } from "react";
 import Grid from '@mui/material/Grid';
 import { Constants } from '../components/Constants';
-import { AppsHeader } from '../styles/apps';
 import ScrollDownButton from '../components/scrollDownButton';
+
+import '../styles/apps.css';
 
 export default function Apps() {
 
@@ -56,32 +57,66 @@ export default function Apps() {
     return (
         <div>
             <Layout>
-                <AppsHeader>apps</AppsHeader>
+                <div className='AppContent'>
+                    <h1 className='AppHeader'>apps</h1>
 
-                <Grid container spacing={0}>
+                    <Grid container spacing={0}>
 
-                    { /* app 1 */}
-                    <Grid item 
-                        xs={Constants.gridSizing.small.left} 
-                        md={Constants.gridSizing.medium.left} 
-                        lg={Constants.gridSizing.large.left}
-                    >
+                        { /* app 1 */}
+                        <Grid item 
+                            xs={Constants.gridSizing.small.left} 
+                            md={Constants.gridSizing.medium.left} 
+                            lg={Constants.gridSizing.large.left}
+                        >
 
-                        <h2>App Image/s</h2>
+                            <div className='AppImages'>
+                                <h2>app image/s</h2>
+                            </div>
+
+                        </Grid>
+
+                        <Grid item 
+                            xs={Constants.gridSizing.small.right} 
+                            md={Constants.gridSizing.medium.right} 
+                            lg={Constants.gridSizing.large.right}
+                        >
+
+                            <div className='AppDescription'>
+                                <h2>app info</h2>
+                                <p>
+                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                                </p>
+                            </div>
+
+                        </Grid>
+
+                        { /* app 2 */}
+                        <Grid item 
+                            xs={Constants.gridSizing.small.left} 
+                            md={Constants.gridSizing.medium.left} 
+                            lg={Constants.gridSizing.large.left}
+                        >
+
+                            <div className='AppImages'>
+                                <h2>app image/s</h2>
+                            </div>
+
+                        </Grid>
+
+                        <Grid item 
+                            xs={Constants.gridSizing.small.right} 
+                            md={Constants.gridSizing.medium.right} 
+                            lg={Constants.gridSizing.large.right}
+                        >
+
+                            <div className='AppDescription'>
+                                <h2>app info</h2>
+                            </div>
+
+                        </Grid>
 
                     </Grid>
-
-                    <Grid item 
-                        xs={Constants.gridSizing.small.right} 
-                        md={Constants.gridSizing.medium.right} 
-                        lg={Constants.gridSizing.large.right}
-                    >
-
-                        <h2>App Info Card</h2>
-
-                    </Grid>
-
-                </Grid>
+                </div>
                 <ScrollIndicator />
             </Layout>
         </div>
